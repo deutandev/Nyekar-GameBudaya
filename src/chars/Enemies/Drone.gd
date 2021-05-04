c2a649b6ed270a4f914ce9049dcad254676176f6
Andika Fathin Azfar <deutandev@pm.me>
2021-05-04 17:24:05 +0700

    Add Enemy AI

--- src/chars/Enemies/Drone.gd
+++ src/chars/Enemies/Drone.gd
@@ -1,16 +1,44 @@
 extends KinematicBody2D
 
 
+onready var animationPlayer = $saber/AnimationPlayer
+
 export var enemyHP = 50
 
+var run_speed = 200
+var velocity = Vector2.ZERO
+var player = null
+var curAnim = "attackLeft"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	if player:
+		velocity = position.direction_to(player.position) * run_speed
+	velocity = move_and_slide(velocity)
+	
+	if velocity.x > 0:
+		$AnimatedSprite.flip_h = true
+		curAnim = "attackRight"
+		animationPlayer.play("attackRight")
+	elif velocity.x < 0:
+		$AnimatedSprite.flip_h = false
+		curAnim = "attackLeft"
+		animationPlayer.play("attackLeft")
+	else:
+		animationPlayer.stop()
 
 func handle_hit(damage):
 	# print(enemyHP)
 	enemyHP -= damage
 	if enemyHP <= 0:
 		queue_free()
+
+
+func _on_DetectRadius_body_entered(body):
+	player = body
+	return player
+
+
+func _on_DetectRadius_body_exited(body):
+	player = null
