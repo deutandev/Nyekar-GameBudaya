ac47a56d839e01f29a00b79c9185daa430f48778
Andika Fathin Azfar <deutandev@pm.me>
2021-05-17 19:26:38 +0700

    Add Player health bar

--- src/scenes/UI/HUD.gd
+++ src/scenes/UI/HUD.gd
@@ -1,6 +1,8 @@
 extends Control
 
 
+onready var hpbar = $HPBar
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,5 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var playerHP = Global.playerHP
 	$HPLabel.text = "Player HP: " + str(Global.playerHP)
 	$BungaLabel.text = "Jumlah Kembang: " + str(Global.bunga) + "/20"
+	$HPBar.value = playerHP
