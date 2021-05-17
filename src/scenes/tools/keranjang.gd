c9d06424f1ac746218e06c72d3a8bc0ef5438be0
Andika Fathin Azfar <deutandev@pm.me>
2021-05-17 08:27:06 +0700

    Add progress bar while picking up kemang

--- src/scenes/tools/keranjang.gd
+++ src/scenes/tools/keranjang.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 var bunga = null
-
+onready var timer = $PickTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,15 +10,26 @@ func _ready():
 
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_pressed("interact"):
 		visible = true
-		monitoring = true
+		$ProgressBar.value += 1.5
+		# monitoring = true
 	else:
+		timer.stop()
 		monitoring = false
 		visible = false
+		$ProgressBar.value = 0
+	if Input.is_action_just_pressed("interact"):
+		timer.start()
+		print("start")
 
 
 func _on_Keranjang_body_entered(body):
 	if body.is_in_group("bunga"):
 		body.queue_free()
 		Global.bunga += 1
+
+
+func _on_PickTimer_timeout():
+	monitoring = true
+	print(monitoring)
